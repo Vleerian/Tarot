045580a71945c4204fe0f8f0075c934f2865294c
Vleerian R <vleerian@remordias>
2023-08-13 20:20:44 -0400

    Fix Build script

--- build.sh
+++ build.sh
@@ -1,10 +1,13 @@
 dotnet publish -c Release --sc true -r win-x64 -p:PublishSingleFile=true -o output /p:DebugType=None /p:DebugSymbols=false
-zip Tarot-Win64 output/Tarot.exe output/e_sqlite3.dll DeckDB.db
+cp DeckDB.db output/DeckDB.db
+zip Tarot-Win64 output/Tarot.exe output/e_sqlite3.dll output/DeckDB.db
 rm output/*
 dotnet publish -c Release --sc true -r osx-x64 -p:PublishSingleFile=true -o output /p:DebugType=None /p:DebugSymbols=false  
-zip Tarot-Osx64 output/Tarot output/libe_sqlite3.dylib DeckDB.db
+cp DeckDB.db output/DeckDB.db
+zip Tarot-Osx64 output/Tarot output/libe_sqlite3.dylib output/DeckDB.db
 rm output/*
 dotnet publish -c Release --sc true -r linux-x64 -p:PublishSingleFile=true -o output /p:DebugType=None /p:DebugSymbols=false
-zip Tarot-Linux64 output/Tarot output/libe_sqlite3.so DeckDB.db
+cp DeckDB.db output/DeckDB.db
+zip Tarot-Linux64 output/Tarot output/libe_sqlite3.so output/DeckDB.db
 rm output/*
 mv *.zip output/
